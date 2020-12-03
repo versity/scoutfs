a3035582d38b9f12393e91b1e25d8f9a77aad27a
Andy Grover <agrover@versity.com>
2020-12-03 15:49:43 -0800

    Add strdup_or_error()
    Add a helper function to handle the impossible event that strdup fails.
    
    Signed-off-by: Andy Grover <agrover@versity.com>

--- utils/src/parse.h
+++ utils/src/parse.h
@@ -2,11 +2,21 @@
 #define _PARSE_H_
 
 #include <sys/time.h>
+#include <argp.h>
 
 int parse_human(char* str, u64 *val_ret);
 int parse_u64(char *str, u64 *val_ret);
 int parse_s64(char *str, s64 *val_ret);
 int parse_u32(char *str, u32 *val_ret);
 int parse_timespec(char *str, struct timespec *ts);
+
+static inline char* strdup_or_error(const struct argp_state *state, char *str)
+{
+	char *new = strdup(str);
+	if (!new)
+		argp_error(state, "memory allocation failed");
+
+	return new;
+}
 
 #endif
