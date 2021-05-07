versity/scoutfs-kmod-dev

mmap: add support for read only mmap()
Add support for read only mmap().

Signed-off-by: Benjamin LaHaise <bcrl@kvack.org>

src/inode.c
@@ -66,6 +66,7 @@ static void scoutfs_inode_ctor(void *obj)
 {
 	struct scoutfs_inode_info *ci = obj;
 
+	mutex_init(&ci->s_i_mutex);
 	mutex_init(&ci->item_mutex);
 	seqcount_init(&ci->seqcount);
 	ci->staging = false;

versity/scoutfs

kmod/src/inode.c
@@ -75,6 +75,7 @@ static void scoutfs_inode_ctor(void *obj)
 	struct scoutfs_inode_info *si = obj;
 
 	init_rwsem(&si->extent_sem);
+	mutex_init(&si->s_i_mutex);
 	mutex_init(&si->item_mutex);
 	seqcount_init(&si->seqcount);
 	si->staging = false;