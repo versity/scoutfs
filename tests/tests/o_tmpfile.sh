fe33a492c213452e3052c9744e9701dcb22f399e
Zach Brown <zab@versity.com>
2023-01-09 10:13:48 -0800

    Make o_tmpfile test more generic
    The o_tmpfile test only did one thing, clean it up a bit so we can add
    more tests to the file.
    
    Signed-off-by: Zach Brown <zab@versity.com>

--- tests/tests/o_tmpfile.sh
+++ tests/tests/o_tmpfile.sh
@@ -1,15 +1,13 @@
 #
-# Run tmpfile_stage and check the output with hexdump.
+# basic tests of O_TMPFILE
 #
 
 t_require_commands stage_tmpfile hexdump
 
+echo "== stage from tmpfile"
 DEST_FILE="$T_D0/dest_file"
-
 stage_tmpfile $T_D0 $DEST_FILE
-
 hexdump -C "$DEST_FILE"
-
-rm -fr "$DEST_FILE"
+rm -f "$DEST_FILE"
 
 t_pass
