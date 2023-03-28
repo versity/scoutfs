e3e5f397acf130ce89fe77f3bd7d6e90612ccf1b
Zach Brown <zab@versity.com>
2023-03-28 14:28:01 -0700

    Promote userspace btree block initialization
    Signed-off-by: Zach Brown <zab@versity.com>

--- utils/src/btree.h
+++ utils/src/btree.h
@@ -1,6 +1,7 @@
 #ifndef _BTREE_H_
 #define _BTREE_H_
 
+void btree_init_block(struct scoutfs_btree_block *bt, int level);
 void btree_init_root_single(struct scoutfs_btree_root *root,
 			    struct scoutfs_btree_block *bt,
 			    u64 seq, u64 blkno);
