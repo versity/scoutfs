b53288ffdc47485aed85b4d2efa2fca2540c3900
Auke Kok <auke.kok@versity.com>
2024-03-11 10:35:20 -0700

    Superblock checks for meta and data dev.
    We check superblock magic, crc, flags. data device superblock is
    checked but a little less thorough.  We check whether the device is
    still mounted, since that would make checking invalid to begin with.
    Quorum blocks are validated to have sane contents.
    
    We add a global problem counter so we can trivially measure and
    report whether any problem was found at all, instead of iterating
    over all the problems and checking each individual count.
    
    We pick the standard exit code values from `fsck` and mirror their
    intentional behavior. This results in `fsck.scoutfs` can now be
    trivially created by making it a wrapper around `scoutfs check`.
    
    Signed-off-by: Auke Kok <auke.kok@versity.com>
    Signed-off-by: Hunter Shaffer <hunter.shaffer@versity.com>

--- utils/src/check/super.h
+++ utils/src/check/super.h
@@ -3,7 +3,10 @@
 
 extern struct scoutfs_super_block *global_super;
 
-int check_supers(void);
+int check_super_crc();
+int check_supers(int data_fd);
+int super_commit(void);
+int check_super_in_use(int meta_fd);
 void super_shutdown(void);
 
 #endif
