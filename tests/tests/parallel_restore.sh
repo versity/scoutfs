b65b220521fa51419d47c2a81a34459f7345cd9c
Auke Kok <auke.kok@versity.com>
2024-04-15 13:57:18 -0400

    Add basic parallel_restore test script
    This script executes the basic parallel_restore test binary which
    incorporates the parallel restore library. The test binary creates
    a few files with xattrs. After restoring, we mount the filesystem
    and do some basic checks to see that the restore was complete.
    
    Signed-off-by: Auke Kok <auke.kok@versity.com>

---
+++ tests/tests/parallel_restore.sh
@@ -0,0 +1,44 @@
+#
+# validate parallel restore library
+#
+
+t_require_commands scoutfs parallel_restore
+
+SCR="$T_TMPDIR/mnt.scratch"
+mkdir -p "$SCR"
+
+# XXX this is all pretty manual, would be nice to have helpers
+echo "== make small meta fs"
+# meta device just big enough for reserves and the metadata we'll fill
+scoutfs mkfs -A -f -Q 0,127.0.0.1,53000 -m 10G "$T_EX_META_DEV" "$T_EX_DATA_DEV" > $T_TMP.mkfs.out 2>&1 || \
+	t_fail "mkfs failed"
+mount -t scoutfs -o metadev_path=$T_EX_META_DEV,quorum_slot_nr=0 \
+	"$T_EX_DATA_DEV" "$SCR"
+umount "$SCR"
+
+echo "== parallel_restore"
+parallel_restore -m "$T_EX_META_DEV" > /dev/null || t_fail "parallel_restore"
+
+echo "== mount restored filesystem"
+mount -t scoutfs -o metadev_path=$T_EX_META_DEV,quorum_slot_nr=0 \
+	"$T_EX_DATA_DEV" "$SCR"
+
+#
+# This is pretty crude...
+#
+echo "== compare filesystem contents"
+scoutfs statfs -p "$SCR" | grep -v -e 'fsid' -e 'rid'
+find "$SCR" -exec scoutfs list-hidden-xattrs {} \; | wc
+scoutfs search-xattrs -p "$SCR" scoutfs.hide.srch.sam_vol_F01030L6 -p "$SCR" | wc
+
+echo "== unmount small meta fs"
+umount "$SCR"
+
+echo "== ENOSPC"
+scoutfs mkfs -A -f -Q 0,127.0.0.1,53000 -m 10G "$T_EX_META_DEV" "$T_EX_DATA_DEV" > $T_TMP.mkfs.out 2>&1 || \
+	t_fail "mkfs failed"
+parallel_restore -m "$T_EX_META_DEV" -d 600:1000 -f 600:1000 -n 4000000 | grep died 2>&1 && t_fail "parallel_restore"
+
+echo "== cleanup"
+rmdir "$SCR"
+t_pass
