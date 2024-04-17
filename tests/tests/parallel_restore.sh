b14cac7b4c7e5ac5da4190c9c2864bf845d7f43b
Auke Kok <auke.kok@versity.com>
2024-04-17 17:16:29 -0400

    Record `scoutfs df` output.
    We should make sure this isn't wildly off as restore should be entirely
    deterministic in block usage run to run.
    
    To make this consistent, we also limit data size to 60G, which should be
    sufficient on all test systems everywhere.
    
    Signed-off-by: Auke Kok <auke.kok@versity.com>

--- tests/tests/parallel_restore.sh
+++ tests/tests/parallel_restore.sh
@@ -10,7 +10,7 @@ mkdir -p "$SCR"
 # XXX this is all pretty manual, would be nice to have helpers
 echo "== make small meta fs"
 # meta device just big enough for reserves and the metadata we'll fill
-scoutfs mkfs -A -f -Q 0,127.0.0.1,53000 -m 10G "$T_EX_META_DEV" "$T_EX_DATA_DEV" > $T_TMP.mkfs.out 2>&1 || \
+scoutfs mkfs -A -f -Q 0,127.0.0.1,53000 -m 10G -d 60G "$T_EX_META_DEV" "$T_EX_DATA_DEV" > $T_TMP.mkfs.out 2>&1 || \
 	t_fail "mkfs failed"
 mount -t scoutfs -o metadev_path=$T_EX_META_DEV,quorum_slot_nr=0 \
 	"$T_EX_DATA_DEV" "$SCR"
@@ -31,12 +31,13 @@ scoutfs statfs -p "$SCR" | grep -v -e 'fsid' -e 'rid'
 find "$SCR" -exec scoutfs list-hidden-xattrs {} \; | wc
 scoutfs search-xattrs -p "$SCR" scoutfs.hide.srch.sam_vol_F01030L6 -p "$SCR" | wc
 find "$SCR" -type f -name "file-*" | head -n 4 | xargs -n 1 filefrag-gc57857a5 -b4096 -v | grep -e ext: -e eof
+scoutfs df -p "$SCR"
 
 echo "== unmount small meta fs"
 umount "$SCR"
 
 echo "== ENOSPC"
-scoutfs mkfs -A -f -Q 0,127.0.0.1,53000 -m 10G "$T_EX_META_DEV" "$T_EX_DATA_DEV" > $T_TMP.mkfs.out 2>&1 || \
+scoutfs mkfs -A -f -Q 0,127.0.0.1,53000 -m 10G -d 60G "$T_EX_META_DEV" "$T_EX_DATA_DEV" > $T_TMP.mkfs.out 2>&1 || \
 	t_fail "mkfs failed"
 parallel_restore -m "$T_EX_META_DEV" -d 600:1000 -f 600:1000 -n 4000000 | grep died 2>&1 && t_fail "parallel_restore"
 
