2b221c1ba7fa406135e319b265ca610f03d6e4f9
Auke Kok <auke.kok@versity.com>
2025-04-21 13:47:22 -0700

    prandom_bytes and family removed, switch to get_random_bytes variants
    In v6.1-rc5-2-ge9a688bcb193, get_random_u32_below() becomes available and
    can start replacing prandom_bytes_max(). Switch to it where we can.
    
    get_random_bytes() has been available since el7, so also replace
    prandom_bytes() where we're using it.
    
    Signed-off-by: Auke Kok <auke.kok@versity.com>

--- kmod/src/block.c
+++ kmod/src/block.c
@@ -884,7 +884,7 @@ int scoutfs_block_dirty_ref(struct super_block *sb, struct scoutfs_alloc *alloc,
 	hdr->magic = cpu_to_le32(magic);
 	hdr->fsid = cpu_to_le64(sbi->fsid);
 	hdr->blkno = cpu_to_le64(bl->blkno);
-	prandom_bytes(&hdr->seq, sizeof(hdr->seq));
+	get_random_bytes(&hdr->seq, sizeof(hdr->seq));
 
 	trace_scoutfs_block_dirty_ref(sb, le64_to_cpu(ref->blkno), le64_to_cpu(ref->seq),
 				      le64_to_cpu(hdr->blkno), le64_to_cpu(hdr->seq));
