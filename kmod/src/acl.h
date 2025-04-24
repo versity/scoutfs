79e70e8e9c35ecf1e731f2d25e1deb6ee133049c
Auke Kok <auke.kok@versity.com>
2025-04-24 15:01:01 -0400

    POSIX ACL changes.
    The .get_acl() method now gets passed a mnt_idmap arg, and we can now
    choose to implement either .get_acl() or .get_inode_acl(). Technically
    .get_acl() is a new implementation, and .get_inode_acl() is the old.
    That second method now also gets an rcu flag passed, but we should be
    fine either way.
    
    Deeper under the covers however we do need to hook up the .set_acl()
    method for inodes, otherwise setfacl will just fail with -ENOTSUPP. To
    make this not super messy (it already is) we tack on the get_acl()
    changes here.
    
    This is all roughly ca. v6.1-rc1-4-g7420332a6ff4.
    
    Signed-off-by: Auke Kok <auke.kok@versity.com>

--- kmod/src/acl.h
+++ kmod/src/acl.h
@@ -1,9 +1,14 @@
 #ifndef _SCOUTFS_ACL_H_
 #define _SCOUTFS_ACL_H_
 
+#ifdef KC_GET_ACL_DENTRY
+struct posix_acl *scoutfs_get_acl(KC_VFS_NS_DEF struct dentry *dentry, int type);
+int scoutfs_set_acl(KC_VFS_NS_DEF struct dentry *dentry, struct posix_acl *acl, int type);
+#else
 struct posix_acl *scoutfs_get_acl(struct inode *inode, int type);
-struct posix_acl *scoutfs_get_acl_locked(struct inode *inode, int type, struct scoutfs_lock *lock);
 int scoutfs_set_acl(struct inode *inode, struct posix_acl *acl, int type);
+#endif
+struct posix_acl *scoutfs_get_acl_locked(struct inode *inode, int type, struct scoutfs_lock *lock);
 int scoutfs_set_acl_locked(struct inode *inode, struct posix_acl *acl, int type,
 			   struct scoutfs_lock *lock, struct list_head *ind_locks);
 #ifdef KC_XATTR_STRUCT_XATTR_HANDLER
