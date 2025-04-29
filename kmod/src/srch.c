1c15f6ef390a262738f5a96273612a7e69dad6e9
Auke Kok <auke.kok@versity.com>
2025-04-29 13:19:24 -0400

    Add sysfs default_groups usage
    Since v5.1-rc3-29-gaa30f47cf666, and in el9, there are changes to reduce
    the amount of boilerplate code needed to hook up lots of attribute files
    using a .default_groups member. In el10, this is the required method as
    .default_attrs has been removed. This touches every sysfs part that we
    have.
    
    Signed-off-by: Auke Kok <auke.kok@versity.com>

--- kmod/src/srch.c
+++ kmod/src/srch.c
@@ -2350,6 +2350,9 @@ static struct attribute *srch_attrs[] = {
 	SCOUTFS_ATTR_PTR(compact_delay_ms),
 	NULL,
 };
+#ifdef KC_KOBJECT_DEFAULT_GROUPS
+ATTRIBUTE_GROUPS(srch);
+#endif
 
 void scoutfs_srch_destroy(struct super_block *sb)
 {
@@ -2391,7 +2394,8 @@ int scoutfs_srch_setup(struct super_block *sb)
 
 	sbi->srch_info = srinf;
 
-	ret = scoutfs_sysfs_create_attrs(sb, &srinf->ssa, srch_attrs, "srch");
+	ret = scoutfs_sysfs_create_attrs(sb, &srinf->ssa, KC_KOBJ_DEFAULT(srch),
+					 "srch");
 	if (ret < 0)
 		goto out;
 
