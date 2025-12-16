100ea47bf74fb451bc06cddc2d63b35eada3db1b
Greg Cymbalski <greg.cymbalski@versity.com>
2025-12-15 18:41:00 -0800

    Clarity: set SKIP_CACHE=1 to ignore docker cache

--- build-container.sh
+++ build-container.sh
@@ -11,7 +11,12 @@ export EL_VER="${EL_VER:-${EL_MAJOR_VER}}"
 export MAJOR_VER="${EL_VER%%.*}"
 export MINOR_VER="${EL_VER#*.}"
 export IS_EDGE="${IS_EDGE:-0}"
+export SKIP_CACHE="${SKIP_CACHE:-0}"
 export FORCE_REBUILD_DOCKER_IMAGE="${FORCE_REBUILD_DOCKER_IMAGE:-0}"
+docker_args=()
+if [ "${SKIP_CACHE}" -eq '1' ]; then
+  docker_args+=(--no-cache)
+fi
 
 if [ -z "${MINOR_VER}" ] || [ -z "${MAJOR_VER}" ]; then
   echo "Major/minor versions could not be inferred from required version ${EL_VER}, bailing out"
@@ -28,9 +33,5 @@ fi
 
 # build fresh 'builder' images only if we don't have them or want to force a rebuild
 if [ "$(docker images -q scoutfs-builder:el${MAJOR_VER}.${MINOR_VER})" == "" ] || [ "${FORCE_REBUILD_DOCKER_IMAGE}" == '1' ]; then
-  docker_args=()
-  if [[ "${SKIP_CACHE}" == 'true' ]]; then
-    docker_args+=(--no-cache)
-  fi
-  docker build . "${docker_args[@]}" --build-arg IS_EDGE="${IS_EDGE}" --build-arg IMAGE_SOURCE="${IMAGE_BASE}:${IMAGE_VERSION}" -t "scoutfs-builder:el${MAJOR_VER}.${MINOR_VER}"
+  docker build . "${docker_args[@]}" --progress plain --build-arg IS_EDGE="${IS_EDGE}" --build-arg IMAGE_SOURCE="${IMAGE_BASE}:${IMAGE_VERSION}" -t "scoutfs-builder:el${MAJOR_VER}.${MINOR_VER}"
 fi
