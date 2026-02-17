57785066c07b20824b4f6550be21173bb5ed2a47
Zach Brown <zab@versity.com>
2026-02-17 13:50:13 -0800

    Add raw_read_inode_info ioctl
    Add an ioctl for reading inode metadata (inode struct and xattrs) in
    bulk and without cluster locking.
    
    Signed-off-by: Zach Brown <zab@versity.com>

--- kmod/src/raw.h
+++ kmod/src/raw.h
@@ -4,5 +4,7 @@
 int scoutfs_raw_read_meta_seq(struct super_block *sb,
 			      struct scoutfs_ioctl_raw_read_meta_seq *rms,
 			      struct scoutfs_ioctl_meta_seq *last_ret);
+int scoutfs_raw_read_inode_info(struct super_block *sb,
+				struct scoutfs_ioctl_raw_read_inode_info *rii);
 
 #endif
